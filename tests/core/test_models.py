5d696efca2a216abfcc2f950dae2fcb57e6b4293
antos07 <trotsenko.ant07@gmail.com>
2025-04-17 12:50:49 +0200

    Add User model with migrations and basic tests
    This commit introduces a `User` SQLAlchemy model, Alembic migrations for creating the associated database table, and basic unit tests for key functionality. Dependency updates include adding `freezegun`, `pytest-freezer`, and `python-dotenv`.

---
+++ tests/core/test_models.py
@@ -0,0 +1,32 @@
+import datetime
+
+import freezegun
+
+from app.core.models import User
+
+RANDOM_DATETIME = datetime.datetime(2025, 1, 17, 17, 17, 17)
+
+
+class TestUser:
+    def test_created_at_defaults_to_current_timestamp(self):
+        with freezegun.freeze_time(RANDOM_DATETIME):
+            user = User(id=1, first_name="test")
+
+        assert user.created_at == RANDOM_DATETIME
+
+    def test_last_activity_at_defaults_to_current_timestamp(
+        self, freezer: freezegun.api.FrozenDateTimeFactory
+    ):
+        freezer.move_to(RANDOM_DATETIME)
+
+        user = User(id=1, first_name="test")
+        assert user.last_activity_at == RANDOM_DATETIME
+
+    def test_update_last_activity(self, freezer: freezegun.api.FrozenDateTimeFactory):
+        user = User(id=1, first_name="test", last_activity_at=RANDOM_DATETIME)
+        new_activity_at = RANDOM_DATETIME + datetime.timedelta(days=1)
+        freezer.move_to(new_activity_at)
+
+        user.update_last_activity()
+
+        assert user.last_activity_at == new_activity_at
