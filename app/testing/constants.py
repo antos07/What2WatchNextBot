21d1c6c0d5184b50d51368fd648a106bca11b462
antos07 <trotsenko.ant07@gmail.com>
2025-04-18 22:34:27 +0200

    Add tests for the app config creation

--- app/testing/constants.py
+++ app/testing/constants.py
@@ -2,3 +2,7 @@ import datetime
 
 BOT_TOKEN = "12345:AAAAAAAAAAAAA"
 RANDOM_DATETIME = datetime.datetime(2025, 1, 17, 17, 17, 17)
+
+
+REDIS_URL = "redis://localhost:6379/0"
+DB_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/postgres"
